9733124799dc711ae5fc127eef07a74ccbb9f0f1
Hedreon <39467046+Hedreon@users.noreply.github.com>
2026-09-10 16:53:27 +0100

    refactor(Minimenu): remove unused import

--- common/src/main/java/org/minimalmenu/common/Minimenu.java
+++ common/src/main/java/org/minimalmenu/common/Minimenu.java
@@ -6,7 +6,6 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.minimalmenu.common.helpers.ScreenExtensions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
